4b090be8341d3de8bf852ea1e38f681fbd2b3755
Robert Strohmeyer <robert@strohmeyer.org>
2014-04-30 21:30:02 -0700

    first commit

---
+++ js/boozio.js
@@ -0,0 +1,42 @@
+Boozio = Ember.Application.create();
+
+Boozio.ApplicationAdapter = DS.FirebaseAdapter.extend({
+  firebase: new Firebase('https://boozio.firebaseio.com')
+});
+
+Boozio.ApplicationSerializer = DS.FirebaseSerializer.extend();
+
+Boozio.Router.map(function() {
+  this.route('drinks', {path: '/'});
+});
+
+Boozio.DrinksRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('drink');
+  }
+});
+
+Boozio.Drink = DS.Model.extend({
+  drinkName: DS.attr('string'),
+});
+
+Boozio.DrinksController = Ember.ArrayController.extend({
+  actions: {
+    createDrink: function() {
+      //Get the drink name set by the 'New Drink' text field
+      var drinkName = this.get('newDrinkName');
+      if (!drinkName.trim()) {return; }
+
+      // create the new drink model
+      var drink = this.store.createRecord('drink', {
+        drinkName: drinkName,
+      });
+
+      // Clear the 'New Drink' field
+      this.set('newDrinkName', '');
+
+      //save the new drink model
+      drink.save();
+    }
+  }
+});
